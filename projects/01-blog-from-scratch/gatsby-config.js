5926e183a1615a3073bcac1220a0929c550da783
Thomas Kim <35410212+th-km@users.noreply.github.com>
2020-01-10 15:24:18 +0100

    Added SEO close #2

--- projects/01-blog-from-scratch/gatsby-config.js
+++ projects/01-blog-from-scratch/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-mdx`,
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-theme-ui",
   ],
 }
--- projects/01-blog-from-scratch/src/components/Layout.js
+++ projects/01-blog-from-scratch/src/components/Layout.js
@@ -2,39 +2,50 @@
 import { Fragment } from "react"
 import { jsx } from "theme-ui"
 import { Global } from "@emotion/core"
+import Helmet from "react-helmet"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import useSiteMetadata from "../hooks/useSiteMetadata"
 
-const Layout = ({ children }) => (
-  <Fragment>
-    <Global
-      styles={theme => ({
-        "*, *::before, *::after": {
-          boxSizing: "border-box",
-        },
-        body: {
-          margin: 0,
-          color: theme.colors.black,
-          backgroundColor: theme.colors.gray[2],
-          fontFamily: theme.fonts.sans,
-          lineHeight: theme.lineHeights.normal,
-        },
-      })}
-    />
-    <div
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        maxWidth: "6xl",
-        minHeight: "100vh",
-        mx: "auto",
-      }}
-    >
-      <Header />
-      {children}
-      <Footer />
-    </div>
-  </Fragment>
-)
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+
+  return (
+    <Fragment>
+      <Global
+        styles={theme => ({
+          "*, *::before, *::after": {
+            boxSizing: "border-box",
+          },
+          body: {
+            margin: 0,
+            color: theme.colors.black,
+            backgroundColor: theme.colors.gray[2],
+            fontFamily: theme.fonts.sans,
+            lineHeight: theme.lineHeights.normal,
+          },
+        })}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <div
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "6xl",
+          minHeight: "100vh",
+          mx: "auto",
+        }}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </Fragment>
+  )
+}
 
 export default Layout
