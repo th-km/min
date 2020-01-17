25af5473760608b4f5f914dbf863123d9f1874dd
Thomas Kim <35410212+th-km@users.noreply.github.com>
2020-01-17 14:36:50 +0100

    Added manifest and offline support

--- projects/01-blog-from-scratch/gatsby-config.js
+++ projects/01-blog-from-scratch/gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Minimal blog",
+        short_name: "Minimal",
+        start_url: `/`,
+        background_color: "#1e1e1e",
+        theme_color: `#fafafa`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "posts",
